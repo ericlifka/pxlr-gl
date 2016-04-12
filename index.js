c531b005b650443b15ec09bd5301c99a8799f762
Eric Lifka <eric.lifka@gmail.com>
2016-04-12 17:56:58 -0400

    copy over rendering files from phoenix

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-(function () {
-  return 0;
-}());
+Definemodule('pxlr/gl', function () {
+  return {
+    name: "pxlr-gl",
+    information: "Rendering pipeline for pxlr"
+  }
+});
+
+DefineModule('views/canvas-renderer', function (require) {
+  return require('pxlr/gl//canvas');
+});
